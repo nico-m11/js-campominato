d91cbd13769a8857b207f1393f956b70dd2859b5
Nicola <nicolamelito.11@gmail.com>
2020-10-28 17:39:31 +0100

    aggiunto
    funzione per controllo se il num esiste nell'array

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ var numUtente = [];
 var bigBang = [];
 
 
-function randomNumber(min,max) {
+function genRandom(1,100) {
   return (Math.floor(Math.random() * max) + min);
 }
+
+function validNumber(array,element) {
+  var valid = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array == element) {
+      valid = true;
+    }
+  }
+  return valid;
+}
+
+console.log(valid, array, genRandom);
